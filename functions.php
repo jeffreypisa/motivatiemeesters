884b35b4aa695bbcda4f479f9fdba3959fabcab6
Jeffrey Pisa <jeffrey.pisa@gmail.com>
2022-05-08 11:06:34 +0000

    Feedback

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ include 'lib/includes.php';
 include 'lib/optionspage.php';
 include 'lib/svg.php';
 include 'lib/cpt.php';
-include 'lib/ctax.php';
+// include 'lib/ctax.php';
 include 'lib/wpadmin.php';
 include 'lib/timber.php';
 
